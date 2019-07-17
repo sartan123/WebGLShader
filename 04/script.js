523687b906c601f6bb9bb92c5838b854e32dcc84
sartan8827 <sartan8827@gmail.com>
2019-07-17 22:25:56 +0900

    Added Phong Shading

---
+++ 04/script.js
@@ -0,0 +1,139 @@
+window.onload = function(){
+    var c = document.getElementById("canvas");
+    c.width = 512;
+    c.height = 512;
+
+    var gl = c.getContext("webgl");
+
+    if(!gl){
+        alert("webgl not supported");
+        return;
+    }
+
+    var mat = new matIV();
+    var mMatrix = mat.identity(mat.create());
+    var vMatrix = mat.identity(mat.create());
+    var pMatrix = mat.identity(mat.create());
+    var vpMatrix = mat.identity(mat.create());
+    var mvpMatrix = mat.identity(mat.create());
+    var invMatrix = mat.identity(mat.create());
+
+    var cameraPosition = [0.0, 0.0, 4.0];
+    var cameraPoint = [0.0, 0.0, 0.0];
+    var cameraUp = [0.0, 1.0, 0.0];
+    mat.lookAt(cameraPosition, cameraPoint, cameraUp, vMatrix);
+
+    var fovy = 45.0;
+    var aspect = c.width / c.height;
+    var near = 0.1;
+    var far = 100.0;
+    mat.perspective(fovy, aspect, near, far, pMatrix);
+
+    mat.multiply(pMatrix, vMatrix, vpMatrix);
+    mat.multiply(vpMatrix, mMatrix, mvpMatrix);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clearDepth(1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    var model = sphere(64, 64, 1.0, [0.5, 0.5, 0.5, 1.0]);
+    //var model = square();
+
+    var vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.p), gl.STATIC_DRAW);
+
+    var colorBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.c), gl.STATIC_DRAW);
+
+    var normalBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(model.n), gl.STATIC_DRAW);
+
+    var indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER,  indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Int16Array(model.i), gl.STATIC_DRAW);
+
+    var vertexSource = document.getElementById("vs").textContent;
+    var fragmentSource = document.getElementById("fs").textContent;
+    var programs = getProgram(vertexSource, fragmentSource);
+ 
+    var attLocation = gl.getAttribLocation(programs, "position");
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.enableVertexAttribArray(attLocation);
+    gl.vertexAttribPointer(attLocation, 3, gl.FLOAT, false, 0, 0);
+
+    var attColorLocation = gl.getAttribLocation(programs, "color");
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.enableVertexAttribArray(attColorLocation);
+    gl.vertexAttribPointer(attColorLocation, 4, gl.FLOAT, false, 0, 0);
+
+    var attNormalLocation = gl.getAttribLocation(programs, "normal");
+    gl.bindBuffer(gl.ARRAY_BUFFER, normalBuffer);
+    gl.enableVertexAttribArray(attNormalLocation);
+    gl.vertexAttribPointer(attNormalLocation, 3, gl.FLOAT, false, 0, 0);
+
+    var uniMVPLocation = gl.getUniformLocation(programs, "mvpMatrix");
+    var uniLightLocation = gl.getUniformLocation(programs, "LightDir");
+    var uniInvLocation = gl.getUniformLocation(programs, "invMatrix");
+    var uniEyePositionLocation = gl.getUniformLocation(programs, "EyePosition");
+    var uniEyePointLocation = gl.getUniformLocation(programs, "EyePoint");
+
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+
+    var count = 0;
+
+    render();
+
+    function render(){
+        count++;
+        var radians = (count % 360) * Math.PI / 180;
+        
+        gl.clearColor(0.0, 0.0, 0.0, 1.0);
+        gl.clearDepth(1.0);
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+        mat.identity(mMatrix);
+        mat.multiply(pMatrix, vMatrix, vpMatrix);
+        mat.multiply(vpMatrix, mMatrix, mvpMatrix);
+
+        mat.inverse(mMatrix, invMatrix);
+        
+        gl.uniformMatrix4fv(uniMVPLocation, false, mvpMatrix);
+        gl.uniformMatrix4fv(uniInvLocation, false, invMatrix);
+        gl.uniform3fv(uniLightLocation, [1.0, 1.0, 1.0]);
+        gl.uniform3fv(uniEyePositionLocation, cameraPosition);
+        gl.uniform3fv(uniEyePointLocation, cameraPoint);
+
+        gl.drawElements(gl.TRIANGLES, model.i.length, gl.UNSIGNED_SHORT, 0);
+        gl.flush();
+
+        requestAnimationFrame(render);
+    }
+
+    function getProgram(vertexSource, fragmentSource){
+        var vertexShader = gl.createShader(gl.VERTEX_SHADER);
+        var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+        var programs = gl.createProgram();
+        gl.shaderSource(vertexShader, vertexSource);
+        gl.compileShader(vertexShader);
+        if(!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)){
+            alert(gl.getShaderInfoLog(vertexShader));
+        }
+        gl.attachShader(programs, vertexShader);
+    
+        gl.shaderSource(fragmentShader, fragmentSource);
+        gl.compileShader(fragmentShader);
+        if(!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)){
+            alert(gl.getShaderInfoLog(fragmentShader));
+        }
+        gl.attachShader(programs, fragmentShader);
+    
+        gl.linkProgram(programs);
+        gl.useProgram(programs);
+
+        return programs;
+    }
+}
